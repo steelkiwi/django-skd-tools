82280e6be83f27d0cac8eb54958bf0dcc46b9993
Timofey Trukhanov <trukhanov@steelkiwi.com>
2013-10-18 17:47:23 +0300

    refactor cache_method decorator

--- skd_tools/decorators.py
+++ skd_tools/decorators.py
@@ -14,19 +14,24 @@ def get_cache_key(prefix, *args, **kwargs):
     return '{}_{}'.format(prefix, hash_args_kwargs)
 
 
-def cache_method(func, prefix=''):
+def cache_method(func=None, prefix=''):
     """
     Cache result of function execution into the `self` object (mostly useful in models).
     Calculate cache key based on `args` and `kwargs` of the function (except `self`).
     """
-    @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        cache_key_prefix = prefix or '_cache_{}'.format(func.__name__)
-        cache_key = get_cache_key(cache_key_prefix, *args, **kwargs)
-        if not hasattr(self, cache_key):
-            setattr(self, cache_key, func(self))
-        return getattr(self, cache_key)
-    return wrapper
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            cache_key_prefix = prefix or '_cache_{}'.format(func.__name__)
+            cache_key = get_cache_key(cache_key_prefix, *args, **kwargs)
+            if not hasattr(self, cache_key):
+                setattr(self, cache_key, func(self))
+            return getattr(self, cache_key)
+        return wrapper
+    if func is None:
+        return decorator
+    else:
+        return decorator(func)
 
 
 def cache_func(prefix, method=False):
