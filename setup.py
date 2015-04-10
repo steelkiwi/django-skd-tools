594b419dced318acc86bf655e116a17ba65c6a17
Andrii Bespalko <bespalko@steelkiwi.com>
2015-04-10 15:11:44 +0300

    Updates module version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="django-skd-tools",
-    version="0.2.1",
+    version="0.2.2",
     author="Steelkiwi",
     author_email="vilisov@steelkiwi.com",  # temp
     url="https://github.com/steelkiwi/django-skd-tools",
